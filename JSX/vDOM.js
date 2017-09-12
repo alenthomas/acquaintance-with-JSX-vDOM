b82e4a1a5f0b78e0403df2a6fde7ac24ba7e85a1
Alen Thomas <alenthomas9@gmail.com>
2017-09-12 10:30:17 +0530

    Adds a 'h' function to convert JSX to JSON objects

---
+++ JSX/vDOM.js
@@ -0,0 +1,16 @@
+/** @jsx h */
+function h(type, props, ...children) {
+  return {type, props, children}
+}
+
+//html
+//<ul class='list'>
+//  <li>item 1</li>
+//  <li>item 2</li>
+//</ul>
+
+// html to javascript objects
+// { type: 'ul', props: { 'class': 'list'}, children: [
+//   { type: 'li', props: {}, children: ['item 1'] },
+//   { type: 'li', props: {}, children: ['item 2'] }
+// ]}
