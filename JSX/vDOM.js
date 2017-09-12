ae9369780d89e47eb7718309eba27f43c17fe9a8
Alen Thomas <alenthomas9@gmail.com>
2017-09-12 11:32:15 +0530

    Adds function to check if node are changed

--- JSX/vDOM.js
+++ JSX/vDOM.js
@@ -34,6 +34,12 @@ function updateElement(rDom, newNode, oldNode, index = 0) {
   }
 }
 
+function changed(node1, node2) {
+  return typeof node1 !== typeof node2 ||
+    typeof node1 === 'string' && node1 !== node2 ||
+    node1.type !== node2.type
+}
+
 const list = (
   <ul className='list'>
     <li>item 1</li>
