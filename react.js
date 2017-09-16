e896507fd5115cf316cafa28767a7096fb7a0322
Alen Thomas <alenthomas9@gmail.com>
2017-09-16 16:09:22 +0530

    Adds RootComponent

--- react.js
+++ react.js
@@ -7,3 +7,11 @@ const h = (type, props={}, children=[]) => ({
 });
 
 const WelcomeComponent = ({ name }) => h('div', {}, [`Welcome ${name}`]);
+
+const RootComponent = ({ user }) => {
+  if (user) {
+    return h(WelcomeComponent, { name: user.name });
+  } else {
+    return h('div', {}, ['Please Login']);
+  }
+};
