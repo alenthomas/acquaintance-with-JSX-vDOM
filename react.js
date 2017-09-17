05b3e143153595da522475ddbc520371fc4a847d
Alen Thomas <alenthomas9@gmail.com>
2017-09-17 13:43:28 +0530

    Updates readme and adds some code to react

--- react.js
+++ react.js
@@ -6,6 +6,17 @@ const h = (type, props={}, children=[]) => ({
   children
 });
 
+const shallowEqual = function (left, right) {
+  if (!left || !right) {
+    return false;
+  }
+  const leftKeys = Object.keys(left);
+  const rightKeys = Object.keys(right);
+
+  return leftKeys.length === rightKeys.length &&
+    leftKeys.every(leftKey => left[leftKey] === right[leftKey]);
+};
+
 const memoize = component => {
   let lastProps = null;
   let lastResult = null;
@@ -23,6 +34,99 @@ const memoize = component => {
   };
 };
 
+const diff = (left, right, patches, parent=null) => {
+  if (!left) {
+    patches.push({
+      parent,
+      type: PatchTypes.PATCH_CREATE_NODE,
+      node: right
+    });
+  } else if (!right) {
+    patches.push({
+      type: PatchTypes.PATCH_REMOVE_NODE,
+      node: left
+    });
+  } else if (left.type !== right.type) {
+    patches.push({
+      type: PatchTypes.PATCH_REPLACE_NODE,
+      replacingNode: left,
+      node: right
+    });
+  } else if (right.memoized) {
+    return;
+  } else {
+    const children = left.children.length >= right.children.length ?
+      left.children : right.children;
+
+    children.forEach((child, index) => diff(
+      left.children[index],
+      right.children[index],
+      patches,
+      left
+    ));
+  }
+};
+
+const ID_KEY = 'data-react-id';
+
+const correlateVDOMNODE = (vdomNode, domRoot) => {
+  if (vdomNode === null) {
+    return domRoot;
+  } else {
+    return document.querySelector(`[${ID_KEY}="${vdomNode.id}"]`);
+  }
+};
+
+const createNodeRecursive = (vdomNode, domNode) => {
+  const domElement = document.createElement(vdomNode.type);
+  domElement.setAttribute(ID_KEY, vdomNode.id);
+  domNode.appendChild(domElement);
+  vdomNode.children.forEach((child) =>
+    createNodeRecursive(child, domElement));
+};
+
+const applyPatch = (patch, domRoot) => {
+  switch(patch.type) {
+    case PatchTypes.PATCH_CREATE_NODE: {
+      const domNode = correlateVDOMNode(patch.parent, domRoot);
+      createNodeRecursive(patch.node, domNode);
+    }
+    break;
+
+    case PatchTypes.PATCH_REMOVE_NODE: {
+      const domNode = correlateVDOMNode(patch.node, domRoot);
+      domNode.parentNode.removeChild(domNode);
+    }
+    break;
+
+    case PatchTypes.PATCH_REPLACE_NODE: {
+      const domNode = correlateVDOMNode(patch.replacingNode, domRoot);
+      const parentDomNode = domNode.parentNode;
+      parentDomNode.removeChild(domNode);
+      createNodeRecursive(patch.node, parentDomNode);
+    }
+    break;
+
+    default:
+      throw new Error(`Missing implementation for patch ${patch.type}`);
+  }
+};
+
+export const createRender = domElement => {
+  let lastVDOM = null;
+  let patches = null;
+
+  return element => {
+    const vdom = createVDOM(element);
+
+    patches = [];
+    diff(lastVDOM, vdom, patches);
+
+    patches.forEach(patch => applyPatch(patch, domElement));
+
+    lastVDOM = vdom;
+  };
+};
 
 export const createVDOM = (element, id='.') => {
   const newElement = {
